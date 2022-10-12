45a76b23a71c46993e81747e828ba9c047d1b305
galnir <nirnir122@gmail.com>
2022-10-12 20:45:26 +0300

    filter products using datepicker

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,71 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+<script lang="ts">
+import json from "./assets/api.json";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+export default {
+  components: { Datepicker },
+  data() {
+    return {
+      json: json.data,
+      start: new Date(),
+      end: new Date(new Date().getTime() + 14 * 24 * 60 * 60 * 1000),
+      date: new Date(),
+    };
+  },
+  methods: {
+    // make sure product is made at the requested day
+    checkProductMade(makeDays: number) {
+      return makeDays < this.date.getDay() + 1;
+    },
+    // make sure product is available on selected date's day
+    checkAvailability(daysArray: number[]) {
+      return daysArray.includes(this.date.getDay() + 1);
+    },
+    // make sure product is not excluded on selected date
+    checkExclusion(
+      dates: {
+        date: string;
+        id: string;
+      }[]
+    ) {
+      return (
+        dates.filter(
+          (obj) =>
+            new Date(obj.date).toString().split(" ").splice(0, 4).join(" ") ===
+            this.date.toString().split(" ").splice(0, 4).join(" ")
+        ).length === 0
+      );
+    },
+  },
+};
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="flex justify-center bg-slate-600 h-screen p-2 overflow-auto">
+    <div class="flex gap-2">
+      <Datepicker
+        v-model="date"
+        :min-date="start"
+        :max-date="end"
+        :clearable="false"
+        week-start="0"
+        prevent-min-max-navigation
+      ></Datepicker>
+      <div class="w-64 md:w-96" dir="rtl">
+        <div v-for="data in json" :key="data.id">
+          <div
+            v-if="
+              checkProductMade(data.times.makeDays) &&
+              checkAvailability(data.times.available_days_of_week) &&
+              checkExclusion(data.times.excludeDates)
+            "
+            class="w-40 md:w-full rounded-md shadow-md bg-white p-4 mb-4"
+          >
+            <h1 class="font-bold text-lg">{{ data.name }}</h1>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
